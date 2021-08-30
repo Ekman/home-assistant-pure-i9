853d81cd1289c21355373e53df080fae69ac8f07
Niklas Ekman <niklas@stockfiller.com>
2021-08-30 18:24:56 +0200

    Simplify the readme

--- custom_components/purei9/vacuum.py
+++ custom_components/purei9/vacuum.py
@@ -29,6 +29,13 @@ def setup_platform(hass, config, add_entities, discovery_info = None) -> None:
     entities = map(PureI9.create, client.getRobots())
     add_entities(entities)
 
+#
+# Definitions of supported features: https://github.com/home-assistant/core/blob/dev/homeassistant/components/vacuum/services.yaml
+# Source code to vacuum entity: https://github.com/home-assistant/core/tree/dev/homeassistant/components/vacuum
+# Source code to entity: https://github.com/home-assistant/core/blob/adab367f0e8c48a68b4dffd0783351b0072fbd0a/homeassistant/helpers/entity.py
+# Documentation to vacuum entity: https://developers.home-assistant.io/docs/core/entity/vacuum/
+# Home Assistant developers: https://developers.home-assistant.io/
+#
 class PureI9(StateVacuumEntity):
     def __init__(
         self,
