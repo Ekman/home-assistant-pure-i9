ebc8f5a4ab0448103d2d72730f811141267686d3
Niklas Ekman <niklas@stockfiller.com>
2021-09-09 21:00:15 +0200

    run tests

---
+++ tests/test_purei9.py
@@ -0,0 +1,54 @@
+"""Test the purei9 module"""
+import unittest
+from purei9_unofficial.common import BatteryStatus, RobotStates
+from homeassistant.components.vacuum import (
+    STATE_CLEANING,
+    STATE_DOCKED,
+    STATE_IDLE
+)
+from purei9
+
+class TestPureI9(unittest.TestCase):
+    """Tests for the purei9 module"""
+    data_state_to_hass = [
+        # No need to test every single case. The test will become too fragile.
+        (RobotStates[1], BatteryStatus[1], STATE_CLEANING),
+        (RobotStates[10], BatteryStatus[6], STATE_DOCKED),
+        (RobotStates[10], BatteryStatus[5], STATE_IDLE)
+    ]
+
+    def test_state_to_hass(self):
+        """Test the state_to_hass function"""
+        for purei9_state, purei9_battery, expected in self.data_state_to_hass:
+            with self.subTest(purei9_state=purei9_state,purei9_battery=purei9_battery):
+                self.assertEqual(expected, purei9.state_to_hass(purei9_state, purei9_battery))
+
+    data_battery_to_hass = [
+        (BatteryStatus[0], 0),
+        (BatteryStatus[1], 20),
+        (BatteryStatus[2], 40),
+        (BatteryStatus[3], 60),
+        (BatteryStatus[4], 80),
+        (BatteryStatus[5], 100)
+    ]
+
+    def test_battery_to_hass(self):
+        """Test the battery_to_hass function"""
+        for purei9_battery, expected in self.data_battery_to_hass:
+            with self.subTest():
+                self.assertEqual(expected, purei9.battery_to_hass(purei9_battery))
+
+    def test_params(self):
+        """Test the Params class"""
+        unique_id = "bar"
+        name = "foo"
+
+        params = purei9.Params(unique_id, name)
+
+        # No need to test every property. The test will become too fragile.
+        self.assertEqual(unique_id, params.unique_id)
+        self.assertEqual(name, params.name)
+        self.assertEqual(100, params.battery)
+
+if __name__ == '__main__':
+    unittest.main()
