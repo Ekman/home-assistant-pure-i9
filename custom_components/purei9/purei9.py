09250e0f11779fb5e2c50bee77edf4f210dd6240
Niklas Ekman <niklas@stockfiller.com>
2021-11-28 14:24:35 +0100

    cleanup in params

--- custom_components/purei9/purei9.py
+++ custom_components/purei9/purei9.py
@@ -52,12 +52,8 @@ def battery_to_hass(pure_i9_battery: str) -> int:
     """Translate Pure i9 data into a Home Assistant battery level"""
     return PURE_I9_BATTERY_MAP.get(pure_i9_battery, 0)
 
-# Should not be specifically disabling properties.
-# Rewrite this class as a TypedDict instead.
-# pylint: disable=too-few-public-methods
 class Params:
     """Data available in the state"""
-    name: str
     battery: int = 100
     state: str = STATE_IDLE
     available: bool = True
