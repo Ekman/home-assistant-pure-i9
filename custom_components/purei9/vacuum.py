3c1b72097dcdd9b8723eb2611efd7a7cc91150bc
Niklas Ekman <niklas@stockfiller.com>
2021-12-04 12:24:10 +0100

    no need for upper dustbin anymore

--- custom_components/purei9/vacuum.py
+++ custom_components/purei9/vacuum.py
@@ -145,7 +145,7 @@ class PureI9(StateVacuumEntity):
 
     @property
     def extra_state_attributes(self) -> Mapping[str, Any]:
-        return {"dustbin": self._params.dustbin.name.upper()}
+        return {"dustbin": self._params.dustbin.name}
 
     def start(self) -> None:
         """Start cleaning"""
