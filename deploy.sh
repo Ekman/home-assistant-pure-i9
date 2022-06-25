cfabccb99845af02a7df616271bee51388343ab9
Niklas Ekman <niklas@stockfiller.com>
2022-06-25 15:21:57 +0200

    Improvements to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 #
 # For easier development, copy the files to your Home Assistant and restart it
@@ -7,5 +7,12 @@
 
 HASS_HOST=${1:-"$HASS_HOST"}
 
-scp -r custom_components/purei9/* "root@$HASS_HOST:~/config/custom_components/purei9" \
-    && ssh "root@$HASS_HOST" 'source /etc/profile.d/homeassistant.sh && ha core restart' 1> /dev/null
+if [ -z "$HASS_HOST" ]; then
+    echo "Missing Home Assistant IP" >&2
+    exit 1
+fi
+
+echo "Connecting to Home Assistant at IP \"$HASS_HOST\""
+
+scp -r custom_components/purei9/* "root@$HASS_HOST:~/config/custom_components/purei9"
+ssh "root@$HASS_HOST" 'source /etc/profile.d/homeassistant.sh && ha core restart' 1> /dev/null
