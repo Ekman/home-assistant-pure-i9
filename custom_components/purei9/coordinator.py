b33397f1a7d51be3e88c6000814b2c6aeb882ee7
Niklas Ekman <nikl.ekman@gmail.com>
2022-12-19 22:11:01 +0100

    lint

--- custom_components/purei9/coordinator.py
+++ custom_components/purei9/coordinator.py
@@ -61,10 +61,10 @@ class PureI9Coordinator(DataUpdateCoordinator):
         params.dustbin = purei9.dustbin_to_hass(purei9_dustbin)
 
         purei9_cleaning_sessions = self._robot.getCleaningSessions()
-        #params.last_cleaning_session = (
-        #    purei9_cleaning_sessions[:0]
-        #    if purei9_cleaning_sessions
-        #    else None
-        #)
+        params.last_cleaning_session = (
+            purei9_cleaning_sessions[:0]
+            if purei9_cleaning_sessions
+            else None
+        )
 
         return params
