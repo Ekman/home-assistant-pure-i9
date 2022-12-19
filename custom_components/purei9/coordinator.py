97b052c7a320449abe95b9b34b8265246f385204
Niklas Ekman <nikl.ekman@gmail.com>
2022-12-19 22:10:40 +0100

    lint

--- custom_components/purei9/coordinator.py
+++ custom_components/purei9/coordinator.py
@@ -31,7 +31,7 @@ class PureI9Coordinator(DataUpdateCoordinator):
     async def _async_update_data(self):
         """Fetch data from Pure i9."""
         try:
-            async with async_timeout.timeout(timedelta(seconds=10)):
+            async with async_timeout.timeout(10):
                 return await self.hass.async_add_executor_job(
                     self.update_and_create_params
                 )
@@ -61,10 +61,10 @@ class PureI9Coordinator(DataUpdateCoordinator):
         params.dustbin = purei9.dustbin_to_hass(purei9_dustbin)
 
         purei9_cleaning_sessions = self._robot.getCleaningSessions()
-        params.last_cleaning_session = (
-            purei9_cleaning_sessions[0]
-            if purei9_cleaning_sessions
-            else None
-        )
+        #params.last_cleaning_session = (
+        #    purei9_cleaning_sessions[:0]
+        #    if purei9_cleaning_sessions
+        #    else None
+        #)
 
         return params
--- custom_components/purei9/vacuum.py
+++ custom_components/purei9/vacuum.py
@@ -141,7 +141,7 @@ class PureI9(CoordinatorEntity, StateVacuumEntity):
                 else None
             ),
             "last_cleaning_stop": (
-                self._params.last_cleaning_session.starttime 
+                self._params.last_cleaning_session.starttime
                     + timedelta(seconds=self._params.last_cleaning_session.duration)
                 if self._params.last_cleaning_session is not None
                 else None
