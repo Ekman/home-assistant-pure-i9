b4bb1fdf8b04ce2f6d98f93377e1d2d522aa932c
Niklas Ekman <nikl.ekman@gmail.com>
2024-01-10 21:24:48 +0100

    fix linting

--- custom_components/purei9/vacuum_command.py
+++ custom_components/purei9/vacuum_command.py
@@ -29,7 +29,7 @@ class CommandCleanZones(CommandBase):
     def name(self) -> str:
         return COMMAND_CLEAN_ZONES
 
-    def valid_or_throw(params: Dict[str, Any]) -> None:
+    def valid_or_throw(self, params: Dict[str, Any]) -> None:
         """Check for required input data"""
         if params is None:
             raise exception.CommandParamException("params", "Dict")
@@ -40,17 +40,19 @@ class CommandCleanZones(CommandBase):
         if not "zones" in params:
             raise exception.CommandParamException("zones", "List")
 
-    async def execute(params: Dict[str, Any]) -> Future:
-        m = utility.first_or_default(self._params.maps, params["map"])
+    async def execute(self, params: Dict[str, Any]) -> Future:
+        map_name = params["map"]
+
+        m = utility.first_or_default(self._params.maps, map_name)
 
         if m is None:
-            raise exception.CommandException("Map \"%s\" does not exist.", params["map"])
+            raise exception.CommandException(f"Map \"{map_name}\" does not exist.")
 
         # Search all zones inside this map for the ones we are looking for
         zone_ids = [zone.id for zone in m.zones if zone.name in params["zones"]]
 
         if len(zone_ids) == 0:
-            raise exception.CommandException("Could not find any zones in map \"%s\".", m.name)
+            raise exception.CommandException(f"Could not find any zones in map \"{m.name}\".")
 
         # Everything done, now send the robot to clean those maps and zones we found
         await self.hass.async_add_executor_job(self.robot.cleanZones, m.id, zone_ids)
