12dacf44cbb8007065d61a4eeed126484a37c0a3
Niklas Ekman <nikl.ekman@gmail.com>
2024-03-01 22:44:34 +0100

    Create array join function. Add tests for new function and first_or_default (#69)

--- custom_components/purei9/utility.py
+++ custom_components/purei9/utility.py
@@ -6,3 +6,12 @@ def first_or_default(collection, predicate, default = None):
             return item
 
     return default
+
+def array_join(array, separator = ", "):
+    """Join an array of strings with a separator"""
+    return separator.join(
+        filter(
+            lambda x: len(x) > 0,
+            [str(item) for item in array if item is not None]
+        )
+    )
