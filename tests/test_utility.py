7ea4d71306511f6c20ee451fe89a03dcbe23e3c2
Niklas Ekman <nikl.ekman@gmail.com>
2024-03-01 22:40:55 +0100

    add test for first_or_default

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -2,8 +2,20 @@
 import unittest
 from custom_components.purei9 import utility
 
-class TestPureI9(unittest.TestCase):
+class TestUtility(unittest.TestCase):
     """Tests for the utility module"""
+    data_first_or_default = [
+        ([1, 2, 3], lambda x: x == 2, None, 2),
+        ([1, 2, 3], lambda x: x == 4, "x", "x"),
+    ]
+
+    def test_first_or_default(self):
+        """Test the first_or_default function"""
+        for collection, predicate, default, expected in self.data_array_join:
+            with self.subTest():
+                self.assertEqual(expected,
+                    utility.first_or_default(collection, predicate, default))
+
     data_array_join = [
         ([1, 2, 3], "-", "1-2-3"),
         (["a", None, "b", 4], "_", "a_b_4"),
