3dfafd7f44584d6ad0f7095a21dad3cb762dabc7
Niklas Ekman <nikl.ekman@gmail.com>
2024-11-23 22:07:42 +0100

    During config, select country code using a selector (#96)

--- custom_components/purei9/config_flow.py
+++ custom_components/purei9/config_flow.py
@@ -1,9 +1,13 @@
 """Initial user configuration for the integration"""
+import logging
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, CONF_COUNTRY_CODE
+from homeassistant.helpers.selector import CountrySelector
 from purei9_unofficial.cloudv3 import CloudClient
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 class HiveOsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Configuration flow"""
@@ -19,6 +23,8 @@ class HiveOsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 password = user_input[CONF_PASSWORD]
                 countrycode = user_input[CONF_COUNTRY_CODE]
 
+                _LOGGER.info("Config flow setup with country code \"%s\".", countrycode)
+
                 purei9_client = CloudClient(email, password, countrycode=countrycode)
                 await self.hass.async_add_executor_job(purei9_client.tryLogin)
 
@@ -33,7 +39,7 @@ class HiveOsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         schema = vol.Schema({
             vol.Required(CONF_EMAIL): str,
             vol.Required(CONF_PASSWORD): str,
-            vol.Required(CONF_COUNTRY_CODE): str,
+            vol.Required(CONF_COUNTRY_CODE): CountrySelector(),
         })
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
