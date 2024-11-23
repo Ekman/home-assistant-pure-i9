c6bdabd25ae3d961b18d7a25d56b17e31c4f8953
Niklas Ekman <nikl.ekman@gmail.com>
2024-11-23 20:16:17 +0100

    Add a reconfigure flow

--- custom_components/purei9/config_flow.py
+++ custom_components/purei9/config_flow.py
@@ -13,20 +13,30 @@ class HiveOsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Configuration flow"""
     VERSION = 1
 
+    def show_form(self, step_id, errors):
+        schema = vol.Schema({
+            vol.Required(CONF_EMAIL): str,
+            vol.Required(CONF_PASSWORD): str,
+            vol.Required(CONF_COUNTRY_CODE): CountrySelector(),
+        })
+
+        return self.async_show_form(step_id=step_id, data_schema=schema, errors=errors)
+
+    def try_login(self, user_input):
+        # Validate that the provided credentials are correct
+        email = user_input[CONF_EMAIL]
+        password = user_input[CONF_PASSWORD]
+        countrycode = user_input[CONF_COUNTRY_CODE]
+
+        purei9_client = CloudClient(email, password, countrycode=countrycode)
+        return self.hass.async_add_executor_job(purei9_client.tryLogin)
+
     async def async_step_user(self, user_input=None):
         errors = {}
 
         if user_input is not None:
             try:
-                # Validate that the provided credentials are correct
-                email = user_input[CONF_EMAIL]
-                password = user_input[CONF_PASSWORD]
-                countrycode = user_input[CONF_COUNTRY_CODE]
-
-                _LOGGER.info("Config flow setup with country code \"%s\".", countrycode)
-
-                purei9_client = CloudClient(email, password, countrycode=countrycode)
-                await self.hass.async_add_executor_job(purei9_client.tryLogin)
+                await self.try_login(user_input)
 
                 return self.async_create_entry(
                     title=email,
@@ -36,10 +46,26 @@ class HiveOsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:
                 errors["base"] = "auth"
 
-        schema = vol.Schema({
-            vol.Required(CONF_EMAIL): str,
-            vol.Required(CONF_PASSWORD): str,
-            vol.Required(CONF_COUNTRY_CODE): CountrySelector(),
-        })
+        return self.show_form("user", errors)
 
-        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+    async def async_step_reconfigure(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            try:
+                await self.try_login(user_input)
+
+                email = user_input[CONF_EMAIL]
+
+                self.async_set_unique_id(email)
+                self._abort_if_unique_id_mismatch()
+
+                return self.async_update_reload_and_abort(
+                    self._get_reconfigure_entry(),
+                    data_updates=user_input,
+                )
+            # pylint: disable=broad-except
+            except Exception:
+                errors["base"] = "auth"
+
+        return self.show_form("reconfigure", errors)
