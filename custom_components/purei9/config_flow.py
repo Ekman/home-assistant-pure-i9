bb4317323fc494beef5bacfe52523da781adaa1f
Niklas Ekman <nikl.ekman@gmail.com>
2024-11-24 16:26:49 +0100

    refactor config flow

--- custom_components/purei9/config_flow.py
+++ custom_components/purei9/config_flow.py
@@ -22,31 +22,35 @@ class HiveOsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(step_id=step_id, data_schema=schema, errors=errors)
 
-    def try_login(self, user_input):
-        # Validate that the provided credentials are correct
-        email = user_input[CONF_EMAIL]
-        password = user_input[CONF_PASSWORD]
-        countrycode = user_input[CONF_COUNTRY_CODE]
+    async def try_login(self, user_input):
+        """Try to login to cloud"""
+        try:
+            # Validate that the provided credentials are correct
+            email = user_input[CONF_EMAIL]
+            password = user_input[CONF_PASSWORD]
+            countrycode = user_input[CONF_COUNTRY_CODE]
 
-        _LOGGER.info("Config flow setup with country code \"%s\".", countrycode)
+            _LOGGER.info("Config flow setup with country code \"%s\".", countrycode)
 
-        purei9_client = CloudClient(email, password, countrycode=countrycode)
-        return self.hass.async_add_executor_job(purei9_client.tryLogin)
+            purei9_client = CloudClient(email, password, countrycode=countrycode)
+            await self.hass.async_add_executor_job(purei9_client.tryLogin)
+
+            return True
+        # pylint: disable=broad-except
+        except Exception ex:
+            _LOGGER.error("Could not step user due to: %s", ex)
+            return False
 
     async def async_step_user(self, user_input=None):
         errors = {}
 
         if user_input is not None:
-            try:
-                await self.try_login(user_input)
-
+            if await self.try_login(user_input):
                 return self.async_create_entry(
                     title=email,
                     data=user_input
                 )
-            # pylint: disable=broad-except
-            except Exception as ex:
-                _LOGGER.error("Could not step user due to: %s", ex)
+            else:
                 errors["base"] = "auth"
 
         return self.show_form("user", errors)
@@ -55,21 +59,17 @@ class HiveOsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            try:
-                email = user_input[CONF_EMAIL]
+            email = user_input[CONF_EMAIL]
 
-                self.async_set_unique_id(email)
-                self._abort_if_unique_id_mismatch()
+            self.async_set_unique_id(email)
+            self._abort_if_unique_id_mismatch()
 
-                await self.try_login(user_input)
-
+            if await self.try_login(user_input)
                 return self.async_update_reload_and_abort(
                     self._get_reconfigure_entry(),
                     data_updates=user_input,
                 )
-            # pylint: disable=broad-except
-            except Exception as ex:
-                _LOGGER.error("Could not step reconfigure due to: %s", ex)
+            else:
                 errors["base"] = "auth"
 
         return self.show_form("reconfigure", errors)
